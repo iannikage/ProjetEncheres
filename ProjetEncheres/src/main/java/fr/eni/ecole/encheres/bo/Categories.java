9ee6b21987d99d8a38ce805d75ce141e5290725e
sarahdoz <sarah.dozeville@aol.com>
2022-12-20 15:15:37 +0100

    Classe Catégories

---
+++ ProjetEncheres/src/main/java/fr/eni/ecole/encheres/bo/Categories.java
@@ -0,0 +1,45 @@
+package fr.eni.ecole.encheres.bo;
+
+public class Categories {
+
+	private int noCategorie;
+	private String libelle;
+	
+	
+	public Categories () {
+	
+	}
+
+	public Categories (int noCategorie, String libelle) {
+		super();
+		this.noCategorie = noCategorie;
+		this.libelle = libelle;
+	}
+
+	public int getNoCategorie() {
+		return noCategorie;
+	}
+
+	public void setNoCategorie(int noCategorie) {
+		this.noCategorie = noCategorie;
+	}
+
+	public String getLibelle() {
+		return libelle;
+	}
+
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+
+	@Override
+	public String toString() {
+		return "Categories [noCategorie=" + noCategorie + ", libelle=" + libelle + "]";
+	}
+	
+	
+	
+	
+
+}
+
